fb825e2b168db46de5c1366dd1f68e0d633b5c55
theo <agen.theo0@gmail.com>
2023-10-25 10:50:16 +0200

    try update v2

--- src/projects/adapters/secondary/inMemory/projects.inMemory.ts
+++ src/projects/adapters/secondary/inMemory/projects.inMemory.ts
@@ -21,9 +21,18 @@ export class InMemoryProjects implements IProjectsRepository {
         return newProject;
     }
 
-    async updateProject(project: IProjectCard): Promise<void> {
-        const index = this.projects.findIndex(p => p._id === project._id)
-        this.projects[index] = project;
+    async updateProject(id: string, project: IProjectCard): Promise<IProjectCard> {
+        const projectIndex = this.projects.findIndex(p => p._id === id);
+    
+        if (projectIndex !== -1) {
+            this.projects[projectIndex] = {
+                ...project,
+                _id: id,
+            };
+            return this.projects[projectIndex];
+        } else {
+            throw new Error('Projet introuvable pour la mise à jour');
+        }
     }
 
     async deleteProject(id: string): Promise<void> {
